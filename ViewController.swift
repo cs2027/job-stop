adfd3a83ed29b04938638f3c0ed6dec79cc136aa
Christopher Song <chris4728.song2001@gmail.com>
2021-01-16 21:50:17 -0600

    Initial Commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobStop
+//
+//  Created by Christopher Song on 1/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
